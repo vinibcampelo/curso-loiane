4a7821553c50c53ddd44f2feddc1e9092fee9d73
Vinicius <vinibcampelo@hotmail.com>
2020-07-16 09:01:38 -0300

    Data binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'https://loiane.training';
+  urlImage: string = 'http://lorempixel.com/400/200/nature/';
+  cursoAngular: boolean = true;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
