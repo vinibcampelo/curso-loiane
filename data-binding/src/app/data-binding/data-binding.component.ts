cbe614e8eeb191adf98ad8c11900840a4aeaed3e
Vinicius <vinibcampelo@hotmail.com>
2020-07-29 08:49:27 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,13 +10,33 @@ export class DataBindingComponent implements OnInit {
   url: string = 'https://loiane.training';
   urlImage: string = 'http://lorempixel.com/400/200/nature/';
   cursoAngular: boolean = true;
+  valorSalvo: string = '';
+  valorAtual: string = '';
+
+  isMouseOver: boolean = false;
 
   getValor() {
     return 1;
   }
 
+  botaoClicado(){
+    alert("Botão clicado");
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
   getCurtirCurso() {
     return true;
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
