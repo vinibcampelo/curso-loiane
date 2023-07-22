de2c66308de809cf1f9d776c41259783645c2982
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2023-07-21 22:29:58 -0300

    serviços

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
+
+  nomePortal: string;
+  cursos: string[] = [];
+
+  constructor(private cursosService: CursosService){
+    this.nomePortal = 'https://loiane.training/';
+    this.cursos = this.cursosService.getCursos();
+  }
 
 }
