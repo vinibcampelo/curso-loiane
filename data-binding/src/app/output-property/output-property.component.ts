2bdc7860cc29ebe510b96ebe148bab488a9afc2e
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2023-08-03 13:51:37 -0300

    acesso ao dom e ao template com view child

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -6,20 +6,30 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./output-property.component.css'],
   // outputs: ['mudouValor']
 })
-export class OutputPropertyComponent {
+export class OutputPropertyComponent implements OnInit {
 
   @Input() valor: number = 0; 
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput!: ElementRef;
+
   incrementa() {
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa() {
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
+  }
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
 }
