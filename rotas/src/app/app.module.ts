b7da174cd0354359b3544b49c53c43b4bdd4cd9a
Vinicius Campelo <vinibcampelo@gmail.com>
2023-09-28 17:54:44 -0300

    rotas simples

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { routing } from './app.routing';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
+
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { CursosComponent } from "./cursos/cursos.component";
+
+const APP_ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: '', component: HomeComponent }
+]
+
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTES);
