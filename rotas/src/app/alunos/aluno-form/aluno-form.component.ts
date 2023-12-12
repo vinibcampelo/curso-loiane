007241c149d56a89c8cc4a7c8478d25ee4fc1a43
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2023-12-12 04:54:36 -0300

    Iniciando a criação do formulário do aluno

--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent {
+export class AlunoFormComponent implements OnInit, OnDestroy {
+
+  aluno: any;
+  inscricao: Subscription = new Subscription();
+
+  constructor(
+    private route: ActivatedRoute,
+    private alunosService: AlunosService
+  ){}
+
+  ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let idAluno = params['id'];
+        this.aluno = this.alunosService.getAluno(idAluno)
+
+        if (this.aluno === null) {
+          this.aluno = {}
+        }
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
 
 }
