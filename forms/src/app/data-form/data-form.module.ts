1b4e2881eca648986ef9c764f1d6969bd22ab6c5
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2024-01-01 17:36:05 -0300

    Sincronizando o html com o form group

--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DataFormComponent } from './data-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -12,7 +13,7 @@ import { DataFormComponent } from './data-form.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    
+    SharedModule
   ],
 })
 export class DataFormModule { }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    TemplateFormComponent,
-    FormDebugComponent
+    TemplateFormComponent  
   ],
   imports: [
     CommonModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ]
 })
 export class TemplateFormModule { }
