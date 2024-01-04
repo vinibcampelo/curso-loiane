518b3fda3bf4fb426143069b87c51fd77f84be2e
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2024-01-04 11:12:32 -0300

    aplicando validações

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -20,8 +20,8 @@ export class DataFormComponent implements OnInit {
     // });
 
     this.formulario = formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: [null, [Validators.required, Validators.email]]
     });
   }
 
@@ -32,14 +32,33 @@ export class DataFormComponent implements OnInit {
   onSubmit(){
     console.log(this.formulario.value);
     this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-    .subscribe(dados => {
-      console.log(dados);
-      this.formulario.reset()
-    },
-    (erro: any) => alert("Algo não esperado ocorreu"));
+    .subscribe({
+      next: (dados) => console.log(dados),
+      error: (error) => alert("Algo não esperado ocorreu"),
+      complete: () => this.formulario.reset()
+  });
   }
 
   resetar() {
     this.formulario.reset()
   }
+
+  aplicaValidacao(campo: any) {
+    const campoForm = this.formulario.get(campo);
+    console.log(campoForm);
+    console.log(this.verificaInvalidTouched(campo) || this.verificaValidTouched(campo));
+    return {
+      'was-validated':  this.verificaInvalidTouched(campoForm) || this.verificaValidTouched(campoForm)
+    }
+  }
+
+  verificaInvalidTouched(campo: any):boolean {
+    return campo.invalid && campo.touched;
+  }
+
+  verificaValidTouched(campo: any): boolean {
+    return campo.valid && campo.touched;
+  }
+
+
 }
