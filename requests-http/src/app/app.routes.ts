b7d528306b3a9c03b056a51cff55d325d7a3eabd
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2024-01-31 14:27:15 -0300

    adicionando rota de cursos

--- requests-http/src/app/app.routes.ts
+++ requests-http/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  {
+    path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  }
+];
