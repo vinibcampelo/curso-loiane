30a7ea3924926bc1c70d11069b69b843b69f82c9
Vinicius Barbosa Campelo <vinibcampelo@gmail.com>
2024-02-03 16:09:23 -0300

    iniciando cursos

--- requests-http/src/app/cursos/cursos.module.ts
+++ requests-http/src/app/cursos/cursos.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CursosRoutingModule
+    CursosRoutingModule,
+    RouterModule,
+    RouterOutlet
   ]
 })
 export class CursosModule { }
